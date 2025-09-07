1731b4ba33e3665bca4643d85a371f1f9bc0cfce
Sung-Ya Chang <nptchang@proton.com>
2025-09-08 00:15:13 +0800

    slow down animation

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -36,7 +36,7 @@ void draw_animation(lv_obj_t *canvas) {
 
     lv_animimg_set_src(art, (const void **)anim_imgs, 5); // set this to the number of images
     /* lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS); */
-    lv_animimg_set_duration(art, 5000); // time (ms) of total duration
+    lv_animimg_set_duration(art, 25000); // time (ms) of total duration
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 #else
